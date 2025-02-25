8836a65cbbd4ec934fcc8dc17d0c8a7ef693e23b
NoahErenmalm <smileygamer06@gmail.com>
2025-02-25 08:28:08 +0100

    login
    login page som inte fungerar helt

--- app.rb
+++ app.rb
@@ -8,11 +8,7 @@ require_relative './model.rb'
 enable :sessions
 
 get('/') do
-    slim(:home)
-end
-
-get('/login') do
-    slim(:login)
+    slim(:index)
 end
 
 get('/register') do
@@ -26,10 +22,38 @@ post('/registering') do
     db = SQLite3::Database.new('./db/database.db')
 
     validate_register_account(username, password, db)
+
+    session[:username] = username
     
     redirect('/')
 end
 
-post('logging') do
+get('/login') do
+    slim(:login)
+end
 
+post('/logging') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('./db/database.db')
+    db.results_as_hash = true
+
+    user_data = db.execute("SELECT UserId, Username, Password FROM Users WHERE Username = ?", username).first
+
+    password_digest = BCrypt::Password.create(password)
+
+    if user_data != nil
+        if username == user_data["Username"] && password_digest == user_data["Password"]
+            session[:username] = user_data["Username"]
+            session[:userId] = user_data["UserId"]
+        else
+            session[:login_error] = "Wrong password"
+            redirect('/login')
+        end
+    else
+        session[:login_error] = "Wrong password"
+        redirect('/login')
+    end
+
+    redirect('/')
 end
